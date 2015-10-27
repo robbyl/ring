77067051f10eb0ddc14f50a9650c3ce151b6a948
Robert <robby@example.com>
2015-10-27 23:18:27 +0300

    update

--- app/models/wedding_photo.rb
+++ app/models/wedding_photo.rb
@@ -1,7 +1,10 @@
 class WeddingPhoto < ActiveRecord::Base
   belongs_to :wedding
   has_one :wedding_album, through: :wedding_album_photos
-  has_attached_file :image, :styles => { :thumb => "180x180#" }
+  has_attached_file :image,
+                    :styles => { :thumb => "180x180#" },
+                    :convert_options => {
+                        :thumb => "-quality 75 -strip" }
 
   validates_attachment :image, :presence => true,
                        :content_type => { :content_type => /\Aimage\/.*\Z/ },
