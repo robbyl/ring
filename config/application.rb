8d7170bf0c6e7e229eca47add0f81e3ee910fce1
robby <robbyl@git>
2015-10-28 14:40:07 +0300

    update

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,9 @@ module Ring
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.paths << Rails.root.join("vendor", "assets", "fonts")
+    config.assets.paths << Rails.root.join("vendor", "assets", "images")
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.assets.precompile += %w(*.eot *.svg *.ttf *.woff)
   end
 end
